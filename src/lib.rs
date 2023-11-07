76362b05595fa005716e2c4d4776f153c832fabf
Lexo Liu <me@lexo.cool>
2023-11-07 20:10:39 +0800

    Provide some convenient method for receiving body

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,17 @@ pub mod backend;
 pub use backend::ClientBackend;
 use backend::HyperBackend;
 
+use bytes::Bytes;
+use bytestr::ByteStr;
+use cookie::{Cookie, CookieJar};
+use http::{HeaderName, HeaderValue};
+use http_kit::{header, Method, Request, Response, Uri};
+use hyper::http;
+use once_cell::sync::Lazy;
 use std::fmt::Debug;
 use std::future::{Future, IntoFuture};
 use std::pin::Pin;
 use std::sync::RwLock;
-use std::task::{ready, Poll};
-
-use cookie::{Cookie, CookieJar};
-use http::HeaderValue;
-use http_kit::{header, Method, Request, Response, Uri};
-use hyper::http;
-use once_cell::sync::Lazy;
 
 type DefaultBackend = HyperBackend;
 
@@ -24,9 +24,9 @@ pub struct Client<B = DefaultBackend> {
 }
 
 impl<B: ClientBackend> Client<B> {
-    pub fn cookie(&self, name: &str) -> Option<Cookie<'static>> {
-        let cookies = self.cookies.read().unwrap();
-        cookies.get(name).map(|v| v.clone())
+    pub fn cookie(self, cookie: Cookie<'static>) -> Self {
+        self.set_cookie(cookie);
+        self
     }
 
     pub fn enable_cookie_store(&mut self) {
@@ -37,11 +37,11 @@ impl<B: ClientBackend> Client<B> {
         self.cookie_store = false;
     }
 
-    pub fn set_cookie(&self, cookie: Cookie<'static>) {
+    fn set_cookie(&self, cookie: Cookie<'static>) {
         self.cookies.write().unwrap().add_original(cookie);
     }
 
-    pub async fn send(&mut self, request: Request) -> http_kit::Result<Response> {
+    pub async fn send(&self, request: Request) -> http_kit::Result<Response> {
         RequestBuilder::new(request, self).await
     }
 }
@@ -80,69 +80,92 @@ pub struct RequestBuilder<'a, B> {
     client: &'a Client<B>,
 }
 
-impl<'a, B> RequestBuilder<'a, B> {
+impl<'a, B: ClientBackend> RequestBuilder<'a, B> {
     fn new(request: Request, client: &'a Client<B>) -> Self {
         Self { request, client }
     }
+
+    fn insert_header(&mut self, name: HeaderName, value: HeaderValue) {
+        self.request.insert_header(name, value);
+    }
+
+    pub async fn bytes(self) -> http_kit::Result<Bytes> {
+        let mut response = self.await?;
+        Ok(response.take_body()?.into_bytes().await?)
+    }
+
+    pub async fn text(self) -> http_kit::Result<ByteStr> {
+        let mut response = self.await?;
+        Ok(response.take_body()?.into_string().await?)
+    }
+
+    pub async fn json<T: serde::de::DeserializeOwned>(self) -> http_kit::Result<T> {
+        let mut response = self.await?;
+        Ok(response.take_body()?.into_json().await?)
+    }
+
+    pub async fn form<T: serde::de::DeserializeOwned>(self) -> http_kit::Result<T> {
+        let mut response = self.await?;
+        Ok(response.take_body()?.into_form().await?)
+    }
+
+    pub fn header<V>(mut self, name: HeaderName, value: V) -> Self
+    where
+        V: TryInto<HeaderValue>,
+        V::Error: Debug,
+    {
+        self.insert_header(name, value.try_into().unwrap());
+        self
+    }
 }
 
-pub struct ResponseFuture<'a, B> {
-    client: &'a Client<B>,
-    request: Option<Request>,
-    future: Option<Pin<Box<dyn 'a + Future<Output = http_kit::Result<Response>>>>>,
+pub struct ResponseFuture<'a> {
+    future: Pin<Box<dyn 'a + Future<Output = http_kit::Result<Response>>>>,
 }
 
-impl<'a, B: ClientBackend> Future for ResponseFuture<'a, B> {
+impl<'a> Future for ResponseFuture<'a> {
     type Output = http_kit::Result<Response>;
 
     fn poll(
         mut self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
-        if let Some(mut request) = self.request.take() {
-            if self.client.cookie_store {
-                let cookies = self.client.cookies.read().unwrap();
-                let vec: Vec<String> = cookies.iter().map(|v| v.encoded().to_string()).collect();
-                request.insert_header(
-                    header::COOKIE,
-                    HeaderValue::try_from(vec.join(";")).unwrap(),
-                );
-            }
-
-            self.future = Some(self.client.backend.call_endpoint(request));
-        }
-
-        if let Some(future) = self.future.as_mut() {
-            let mut result = ready!(Pin::new(future).poll(cx));
-            if self.client.cookie_store {
-                result = result.map(|response| {
-                    let mut cookies = self.client.cookies.write().unwrap();
-
-                    for cookie in response.headers().get_all(header::SET_COOKIE) {
-                        let cookie = String::from_utf8(cookie.as_bytes().to_vec()).unwrap();
-                        cookies.add_original(Cookie::parse(cookie).unwrap());
-                    }
-                    response
-                });
-            }
-
-            Poll::Ready(result)
-        } else {
-            panic!("Please do not poll this future after it finished.")
-        }
+        self.future.as_mut().poll(cx)
     }
 }
 
 impl<'a, B: ClientBackend> IntoFuture for RequestBuilder<'a, B> {
     type Output = http_kit::Result<Response>;
 
-    type IntoFuture = ResponseFuture<'a, B>;
+    type IntoFuture = ResponseFuture<'a>;
 
-    fn into_future(self) -> Self::IntoFuture {
+    fn into_future(mut self) -> Self::IntoFuture {
         ResponseFuture {
-            client: self.client,
-            request: Some(self.request),
-            future: None,
+            future: Box::pin(async move {
+                if self.client.cookie_store {
+                    let cookies = self.client.cookies.read().unwrap();
+                    let vec: Vec<String> =
+                        cookies.iter().map(|v| v.encoded().to_string()).collect();
+                    self.request.insert_header(
+                        header::COOKIE,
+                        HeaderValue::try_from(vec.join(";")).unwrap(),
+                    );
+                }
+
+                let mut result = self.client.backend.call_endpoint(&mut self.request).await;
+                if self.client.cookie_store {
+                    result = result.map(|response| {
+                        let mut cookies = self.client.cookies.write().unwrap();
+
+                        for cookie in response.headers().get_all(header::SET_COOKIE) {
+                            let cookie = String::from_utf8(cookie.as_bytes().to_vec()).unwrap();
+                            cookies.add_original(Cookie::parse(cookie).unwrap());
+                        }
+                        response
+                    });
+                }
+                result
+            }),
         }
     }
 }
@@ -163,7 +186,7 @@ mod test {
 
     #[tokio::test]
     async fn example() {
-        let mut client = Client::new();
+        let client = Client::new();
         let mut response = client
             .send(Request::get("http://example.com"))
             .await
