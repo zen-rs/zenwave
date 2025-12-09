35aa1c14fc2717912edbdf30b36758b5d5c2cd60
Lexo Liu <me@lexo.cool>
2025-12-09 10:43:10 +0800

    fix: Remove lint and clippy job in test ci

--- scripts/test-apple.sh
+++ scripts/test-apple.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Running fmt..."
-cargo fmt --all -- --check
-
-echo "Running clippy (apple backend)..."
-cargo clippy --all-targets --no-default-features --features apple-backend --workspace -- -D warnings
-
 echo "Running tests (apple backend)..."
 cargo test --no-default-features --features apple-backend --workspace
